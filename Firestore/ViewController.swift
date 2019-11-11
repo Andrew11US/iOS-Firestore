4ca7d35638ecce9a20ae5f89eac1f3e6d9989920
Andrii Halabuda <losangelesboy11@gmail.com>
2019-11-11 21:26:44 +0100

    Added files

--- Firestore/ViewController.swift
+++ Firestore/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        readFirestore()
+    }
+    
+    func readFirestore() {
+        let db = Firestore.firestore()
+        db.collection("users").getDocuments { (snapshot, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "error")
+            } else {
+                for doc in (snapshot?.documents)! {
+                    let data = doc.data()
+                    print(data)
+//                    print(doc)
+//                    if let name = doc.data()["name"] as? String {
+//                        if let age = doc.data()["age"] as? Int {
+//                            print(name, age)
+//                        }
+//                    }
+                    
+                }
+            }
+        }
+    }
+    
+    func writeToFirestore() {
+        
+        let dict: [String:Any] = ["name": "Drew", "age": 25]
+        
+        let db = Firestore.firestore()
+        db.collection("users").document("Drew").setData(dict)
+        
     }
 
 
